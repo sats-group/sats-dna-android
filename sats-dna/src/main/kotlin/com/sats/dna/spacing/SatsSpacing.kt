e3ac6942686025fa2fdb8f23470755ab174a8e3b
Sindre Moen <sindrenm@gmail.com>
2023-03-03 15:22:19 +0100

    NMA-4881: Add spacing (#5)

---
+++ sats-dna/src/main/kotlin/com/sats/dna/spacing/SatsSpacing.kt
@@ -0,0 +1,13 @@
+package com.sats.dna.spacing
+
+import androidx.compose.ui.unit.dp
+
+object SatsSpacing {
+    val xxs = 4.dp
+    val xs = 8.dp
+    val s = 12.dp
+    val m = 16.dp
+    val l = 24.dp
+    val xl = 32.dp
+    val xxl = 64.dp
+}
