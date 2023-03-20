d1d3d870cc50f723cefc79a4e851304b4755fcc6
Sindre Moen <sindrenm@gmail.com>
2023-03-20 16:01:07 +0100

    Create a sample app with some empty screens

---
+++ sample-app/src/main/kotlin/com/sats/dna/sample/typography/TypographyScreen.kt
@@ -0,0 +1,35 @@
+package com.sats.dna.sample.typography
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.insets.ui.Scaffold
+import com.google.accompanist.insets.ui.TopAppBar
+import com.sats.dna.theme.SatsTheme
+
+@Composable
+internal fun TypographyScreen(navigateUp: () -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = SatsTheme.colors.surface.primary,
+                contentPadding = WindowInsets.statusBars.asPaddingValues(),
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(SatsTheme.icons.back, contentDescription = null)
+                    }
+                },
+                title = { Text("Typography") },
+            )
+        },
+    ) { innerPadding ->
+        Box(Modifier.padding(innerPadding))
+    }
+}
