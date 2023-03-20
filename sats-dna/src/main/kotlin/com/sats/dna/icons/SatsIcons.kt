e65d681d9e6c5e5897e0c88d9e85f2538f16a26e
cecilieopperudsats <cecilie.opperud@sats.no>
2023-03-20 12:13:32 +0100

    Sort lines

--- sats-dna/src/main/kotlin/com/sats/dna/icons/SatsIcons.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/icons/SatsIcons.kt
@@ -20,8 +20,8 @@ import com.sats.dna.theme.SatsTheme
 object SatsIcons {
     val chevronRight @Composable get() = painterResource(R.drawable.ic_chevron_right)
     val comment @Composable get() = painterResource(R.drawable.ic_comment)
-    val linkExternal @Composable get() = painterResource(R.drawable.ic_link_external)
     val fistBump @Composable get() = painterResource(R.drawable.ic_fist_bump)
+    val linkExternal @Composable get() = painterResource(R.drawable.ic_link_external)
     val navActivityFilled @Composable get() = painterResource(R.drawable.ic_nav_activity_filled)
     val navActivityOutlined @Composable get() = painterResource(R.drawable.ic_nav_activity_outlined)
     val navBookFilled @Composable get() = painterResource(R.drawable.ic_nav_book_filled)
@@ -64,8 +64,8 @@ private val allIcons: List<Painter>
     @Composable get() = listOf(
         SatsIcons.chevronRight,
         SatsIcons.comment,
-        SatsIcons.linkExternal,
         SatsIcons.fistBump,
+        SatsIcons.linkExternal,
         SatsIcons.navActivityFilled,
         SatsIcons.navActivityOutlined,
         SatsIcons.navBookFilled,
