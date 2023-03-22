9e3ac782764c348ca5e6358874bd40eeb3d998d7
Sindre Moen <sindrenm@gmail.com>
2023-03-22 17:01:59 +0100

    Animate between loading and label

--- sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButton.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButton.kt
@@ -1,8 +1,11 @@
 package com.sats.dna.components.button
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -10,6 +13,7 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.sats.dna.theme.SatsTheme
@@ -34,17 +38,30 @@ fun SatsButton(
         colors = buttonColors,
         contentPadding = buttonPadding(isLarge),
     ) {
-        if (isLoading) {
-            val color by buttonColors.contentColor(isEnabled)
+        Box(
+            Modifier
+                .height(24.dp)
+                .animateContentSize(),
+            contentAlignment = Center,
+        ) {
+            if (isLoading) {
+                val color by buttonColors.contentColor(isEnabled)
 
-            CircularProgressIndicator(Modifier.size(24.dp), color, 1.5.dp)
-        } else {
-            Text(
-                text = label,
-                modifier = Modifier
-                    .height(24.dp)
-                    .wrapContentHeight(),
-            )
+                CircularProgressIndicator(
+                    Modifier
+                        .fillMaxHeight()
+                        .aspectRatio(1f),
+                    color = color,
+                    strokeWidth = 1.5.dp,
+                )
+            } else {
+                Text(
+                    text = label,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .wrapContentHeight(),
+                )
+            }
         }
     }
 }
