09aef97e43c61f28e0a24eb748d39ec89de975d5
Sindre Moen <sindrenm@gmail.com>
2023-05-19 14:04:41 +0200

    Show button loading indicator next to its label

--- sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButton.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButton.kt
@@ -1,11 +1,13 @@
 package com.sats.dna.components.button
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -13,7 +15,7 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.sats.dna.theme.SatsTheme
@@ -38,30 +40,26 @@ fun SatsButton(
         colors = buttonColors,
         contentPadding = buttonPadding(isLarge),
     ) {
-        Box(
-            Modifier
-                .height(24.dp)
-                .animateContentSize(),
-            contentAlignment = Center,
-        ) {
-            if (isLoading) {
+        Row(Modifier.height(24.dp), horizontalArrangement = spacedBy(0.dp), verticalAlignment = CenterVertically) {
+            AnimatedVisibility(isLoading) {
                 val color by buttonColors.contentColor(isEnabled)
 
                 CircularProgressIndicator(
                     Modifier
-                        .fillMaxHeight()
-                        .aspectRatio(1f),
+                        .padding(end = SatsTheme.spacing.s)
+                        .size(16.dp),
                     color = color,
                     strokeWidth = 1.5.dp,
                 )
-            } else {
-                Text(
-                    text = label,
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .wrapContentHeight(),
-                )
             }
+
+            Text(
+                text = label,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .wrapContentHeight(),
+                maxLines = 1,
+            )
         }
     }
 }
