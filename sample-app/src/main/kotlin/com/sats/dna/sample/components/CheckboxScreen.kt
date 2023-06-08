f655b3a838cbf7f3d359859b73c1c82f222d074d
Sindre Moen <sindrem@gmail.com>
2023-06-08 21:49:50 +0200

    Build regular checkbox component

--- sample-app/src/main/kotlin/com/sats/dna/sample/components/CheckboxScreen.kt
+++ sample-app/src/main/kotlin/com/sats/dna/sample/components/CheckboxScreen.kt
@@ -1,7 +1,7 @@
 package com.sats.dna.sample.components
 
 import androidx.compose.foundation.layout.Arrangement.spacedBy
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,21 +15,34 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.state.ToggleableState
+import com.sats.dna.components.SatsCheckbox
 import com.sats.dna.components.SatsTriStateCheckbox
 import com.sats.dna.theme.SatsTheme
 
 @Composable
 internal fun CheckboxScreen(navigateUp: () -> Unit) {
     ComponentScreen("Checkbox", navigateUp) { innerPadding ->
-        Box(
+        Column(
             Modifier
                 .padding(innerPadding)
                 .padding(SatsTheme.spacing.m)
                 .fillMaxSize()
                 .wrapContentHeight(),
         ) {
+            RegularRow()
             TriStateRow()
         }
+    }
+}
+
+@Composable
+private fun RegularRow() {
+    Row(horizontalArrangement = spacedBy(SatsTheme.spacing.s), verticalAlignment = CenterVertically) {
+        val (isChecked, setIsChecked) = remember { mutableStateOf(false) }
+
+        SatsCheckbox(isChecked, setIsChecked)
+
+        Text("Checked: $isChecked")
     }
 }
 
@@ -40,7 +53,7 @@ private fun TriStateRow() {
 
         SatsTriStateCheckbox(toggleableState, onClick = { toggleableState = toggleableState.next() })
 
-        Text(toggleableState.name)
+        Text("State: ${toggleableState.name}")
     }
 }
 
