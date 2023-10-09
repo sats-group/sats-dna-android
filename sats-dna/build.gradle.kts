47afc7d93328b60222282d859165b9939c853a4e
Sindre Moen <sindrenm@gmail.com>
2023-10-09 16:51:20 +0200

    Add Material 3 dependency

--- sats-dna/build.gradle.kts
+++ sats-dna/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation(libs.accompanist.insetsUi)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
