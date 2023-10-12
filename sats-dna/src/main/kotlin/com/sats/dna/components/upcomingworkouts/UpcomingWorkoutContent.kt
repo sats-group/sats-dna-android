f5794483445b5b9f00f38287058dcb06606ffab4
Sindre Moen <sindrem@gmail.com>
2023-10-12 22:09:48 +0200

    Support Material 3 in Upcoming Workout List Items

--- sats-dna/src/main/kotlin/com/sats/dna/components/upcomingworkouts/UpcomingWorkoutContent.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/upcomingworkouts/UpcomingWorkoutContent.kt
@@ -2,10 +2,10 @@ package com.sats.dna.components.upcomingworkouts
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.sats.dna.components.SatsSurface
+import com.sats.dna.internal.MaterialText
 import com.sats.dna.theme.SatsTheme
 import com.sats.dna.tooling.LightDarkPreview
 
@@ -13,16 +13,19 @@ import com.sats.dna.tooling.LightDarkPreview
 internal fun TimeAndDuration(
     time: String,
     duration: String,
+    useMaterial3: Boolean,
     modifier: Modifier = Modifier,
 ) {
     Column(modifier) {
-        Text(
-            time,
+        MaterialText(
+            useMaterial3 = useMaterial3,
+            text = time,
             style = SatsTheme.typography.medium.basic,
         )
 
-        Text(
-            duration,
+        MaterialText(
+            useMaterial3 = useMaterial3,
+            text = duration,
             style = SatsTheme.typography.default.small,
             color = SatsTheme.colors.onBackground.secondary,
         )
@@ -34,45 +37,55 @@ internal fun WorkoutInfo(
     name: String,
     location: String,
     instructor: String,
+    useMaterial3: Boolean,
     modifier: Modifier = Modifier,
     waitingListStatus: WaitingListStatus? = null,
 ) {
     Column(modifier) {
-        Text(
+        MaterialText(
+            useMaterial3,
             name,
             style = SatsTheme.typography.medium.basic,
         )
 
-        Text(
+        MaterialText(
+            useMaterial3,
             location,
             color = SatsTheme.colors.onSurface.secondary,
             style = SatsTheme.typography.default.small,
         )
 
-        Text(
+        MaterialText(
+            useMaterial3,
             instructor,
             style = SatsTheme.typography.default.small,
             color = SatsTheme.colors.onBackground.secondary,
         )
 
         waitingListStatus?.let { status ->
-            WaitingListStatus(status)
+            WaitingListStatus(status, useMaterial3)
         }
     }
 }
 
 @Composable
-private fun WaitingListStatus(status: WaitingListStatus) {
+private fun WaitingListStatus(status: WaitingListStatus, useMaterial3: Boolean) {
     val color = when (status) {
         is WaitingListStatus.OnWaitingList -> SatsTheme.colors.waitingList.text
         is WaitingListStatus.SpotSecured -> SatsTheme.colors.signalText.success
     }
 
-    Text(status.text, color = color, style = SatsTheme.typography.default.small)
+    MaterialText(
+        useMaterial3 = useMaterial3,
+        text = status.text,
+        color = color,
+        style = SatsTheme.typography.default.small,
+    )
 
     if (status is WaitingListStatus.SpotSecured) {
         status.waitingListText?.let {
-            Text(
+            MaterialText(
+                useMaterial3,
                 text = status.waitingListText,
                 color = SatsTheme.colors.waitingList.text,
                 style = SatsTheme.typography.default.small,
@@ -90,6 +103,7 @@ private fun TimeAndDurationPreview() {
                 time = "9:00 PM",
                 duration = "45 min",
                 modifier = Modifier.padding(SatsTheme.spacing.m),
+                useMaterial3 = true,
             )
         }
     }
@@ -106,6 +120,7 @@ private fun WorkoutInfoPreview() {
                 instructor = "w/ Andrew Nielsen",
                 waitingListStatus = WaitingListStatus.SpotSecured("Spot secured! 32 on the waiting list."),
                 modifier = Modifier.padding(SatsTheme.spacing.m),
+                useMaterial3 = true,
             )
         }
     }
