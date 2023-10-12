419a0e82485adae7bdeb2a5e9b0173f80d3878ce
Sindre Moen <sindrenm@gmail.com>
2023-10-12 10:59:42 +0200

    Migrate switches

--- sample-app/src/main/kotlin/com/sats/dna/sample/components/SwitchScreen.kt
+++ sample-app/src/main/kotlin/com/sats/dna/sample/components/SwitchScreen.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import com.sats.dna.components.SatsSwitch
@@ -28,14 +31,30 @@ internal fun SwitchScreen(navigateUp: () -> Unit) {
             LabeledSwitch("Enabled, selected", enabled = true, selected = true)
             LabeledSwitch("Disabled, unselected", enabled = false, selected = false)
             LabeledSwitch("Disabled, selected", enabled = false, selected = true)
+
+            Divider()
+
+            val (isSelected, onSelectChanged) = remember { mutableStateOf(false) }
+
+            LabeledSwitch(
+                label = "Selected: $isSelected",
+                enabled = true,
+                selected = isSelected,
+                onSelectChanged = onSelectChanged,
+            )
         }
     }
 }
 
 @Composable
-private fun LabeledSwitch(label: String, enabled: Boolean, selected: Boolean) {
+private fun LabeledSwitch(
+    label: String,
+    enabled: Boolean,
+    selected: Boolean,
+    onSelectChanged: ((selected: Boolean) -> Unit)? = null,
+) {
     Row(horizontalArrangement = spacedBy(SatsTheme.spacing.s), verticalAlignment = CenterVertically) {
-        SatsSwitch(selected, null, enabled = enabled)
+        SatsSwitch(selected, onSelectChanged, enabled = enabled)
 
         Text(label)
     }
