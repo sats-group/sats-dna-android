d264fa5ffecb17f2260816c4919c7fa43a3cc848
Sindre Moen <sindrem@gmail.com>
2023-12-09 14:32:10 +0100

    Create fistBump icon helper function

--- sats-dna/src/main/kotlin/com/sats/dna/components/button/LikeButton.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/button/LikeButton.kt
@@ -3,13 +3,11 @@ package com.sats.dna.components.button
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.IconToggleButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.sats.dna.components.SatsSurface
+import com.sats.dna.icons.fistBump
 import com.sats.dna.internal.MaterialIcon
 import com.sats.dna.theme.SatsTheme
 import com.sats.dna.tooling.LightDarkPreview
@@ -20,31 +18,23 @@ fun LikeButton(
     onLikedChange: (isLiked: Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    IconToggleButton(modifier = modifier, checked = isLiked, onCheckedChange = onLikedChange) {
-        if (isLiked) {
-            MaterialIcon(
-                painter = SatsTheme.icons.fistBumpFilled,
-                tint = SatsTheme.colors.action.default,
-                contentDescription = null,
-            )
-        } else {
-            MaterialIcon(
-                painter = SatsTheme.icons.fistBump,
-                tint = SatsTheme.colors.action.default,
-                contentDescription = null,
-            )
-        }
+    IconToggleButton(isLiked, onLikedChange, modifier) {
+        MaterialIcon(
+            SatsTheme.icons.fistBump(isLiked),
+            tint = SatsTheme.colors.action.default,
+            contentDescription = null,
+        )
     }
 }
 
 @LightDarkPreview
 @Composable
 private fun LikedPreview() {
-    var isLiked by remember { mutableStateOf(true) }
+    val (isLiked, setIsLiked) = remember { mutableStateOf(true) }
 
     SatsTheme {
         SatsSurface {
-            LikeButton(isLiked = isLiked, onLikedChange = { isLiked = it }, Modifier.padding(16.dp))
+            LikeButton(isLiked, setIsLiked, Modifier.padding(SatsTheme.spacing.m))
         }
     }
 }
@@ -52,11 +42,11 @@ private fun LikedPreview() {
 @LightDarkPreview
 @Composable
 private fun NotLikedPreview() {
-    var isLiked by remember { mutableStateOf(false) }
+    val (isLiked, setIsLiked) = remember { mutableStateOf(false) }
 
     SatsTheme {
         SatsSurface {
-            LikeButton(isLiked = isLiked, onLikedChange = { isLiked = it }, Modifier.padding(16.dp))
+            LikeButton(isLiked, setIsLiked, Modifier.padding(SatsTheme.spacing.m))
         }
     }
 }
