bce8356ee8171af2d4090e9a1f6835696484c41d
Sindre Moen <sindrenm@gmail.com>
2023-12-20 13:08:59 +0100

    NMA-5941: Add missing CleanSecondary and Action buttons (#283)

--- sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButtonColor.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButtonColor.kt
@@ -7,7 +7,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.sats.dna.theme.SatsTheme
 
-enum class SatsButtonColor { Primary, Cta, Secondary, Clean, WaitingList, WaitingListSecondary, Transparent }
+enum class SatsButtonColor {
+    Cta,
+    Primary,
+    Secondary,
+    Clean,
+    CleanSecondary,
+    WaitingList,
+    WaitingListSecondary,
+    Action,
+    Transparent,
+}
 
 @Composable
 internal fun SatsButtonColor.asMaterialButtonColors(isEnabled: Boolean): ButtonColors {
@@ -38,44 +48,52 @@ internal val SatsButtonColor.disabledBorderColor: Color?
 
 internal val SatsButtonColor.backgroundColor: Color
     @Composable get() = when (this) {
-        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.default.bg
         SatsButtonColor.Cta -> SatsTheme.colors2.buttons.cta.default.bg
+        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.default.bg
         SatsButtonColor.Secondary -> Color.Transparent
         SatsButtonColor.Clean -> SatsTheme.colors2.buttons.clean.default.bg
+        SatsButtonColor.CleanSecondary -> SatsTheme.colors2.buttons.cleanSecondary.default.bg
         SatsButtonColor.WaitingList -> SatsTheme.colors2.buttons.waitingListFilled.default.bg
         SatsButtonColor.WaitingListSecondary -> Color.Transparent
+        SatsButtonColor.Action -> Color.Transparent
         SatsButtonColor.Transparent -> Color.Transparent
     }
 
 internal val SatsButtonColor.contentColor: Color
     @Composable get() = when (this) {
-        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.default.fg
         SatsButtonColor.Cta -> SatsTheme.colors2.buttons.cta.default.fg
+        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.default.fg
         SatsButtonColor.Secondary -> SatsTheme.colors2.buttons.secondary.default.fg
         SatsButtonColor.Clean -> SatsTheme.colors2.buttons.clean.default.fg
+        SatsButtonColor.CleanSecondary -> SatsTheme.colors2.buttons.cleanSecondary.default.fg
         SatsButtonColor.WaitingList -> SatsTheme.colors2.buttons.waitingListFilled.default.fg
         SatsButtonColor.WaitingListSecondary -> SatsTheme.colors2.buttons.waitingListOutlined.default
+        SatsButtonColor.Action -> SatsTheme.colors.action.default
         SatsButtonColor.Transparent -> SatsTheme.colors2.buttons.action.default
     }
 
 internal val SatsButtonColor.disabledBackgroundColor: Color
     @Composable get() = when (this) {
-        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.disabled.bg
         SatsButtonColor.Cta -> SatsTheme.colors2.buttons.cta.disabled.bg
+        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.disabled.bg
         SatsButtonColor.Secondary -> Color.Transparent
         SatsButtonColor.Clean -> SatsTheme.colors2.buttons.clean.disabled.bg
+        SatsButtonColor.CleanSecondary -> SatsTheme.colors2.buttons.cleanSecondary.disabled.bg
         SatsButtonColor.WaitingList -> SatsTheme.colors2.buttons.waitingListFilled.disabled.bg
         SatsButtonColor.WaitingListSecondary -> Color.Transparent
+        SatsButtonColor.Action -> Color.Transparent
         SatsButtonColor.Transparent -> Color.Transparent
     }
 
 internal val SatsButtonColor.disabledContentColor: Color
     @Composable get() = when (this) {
-        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.disabled.fg
         SatsButtonColor.Cta -> SatsTheme.colors2.buttons.cta.disabled.fg
+        SatsButtonColor.Primary -> SatsTheme.colors2.buttons.primary.disabled.fg
         SatsButtonColor.Secondary -> SatsTheme.colors2.buttons.secondary.disabled.fg
         SatsButtonColor.Clean -> SatsTheme.colors2.buttons.clean.disabled.fg
+        SatsButtonColor.CleanSecondary -> SatsTheme.colors2.buttons.cleanSecondary.disabled.fg
         SatsButtonColor.WaitingList -> SatsTheme.colors2.buttons.waitingListFilled.disabled.fg
         SatsButtonColor.WaitingListSecondary -> SatsTheme.colors2.buttons.waitingListOutlined.disabled
+        SatsButtonColor.Action -> SatsTheme.colors.action.disabled
         SatsButtonColor.Transparent -> SatsTheme.colors2.buttons.action.disabled
     }
