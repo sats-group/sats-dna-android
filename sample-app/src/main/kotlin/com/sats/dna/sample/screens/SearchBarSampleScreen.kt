498140c44ddf6566022932699633260e2cd7bfcc
Cecilie Opperud <107850862+cecilieopperudsats@users.noreply.github.com>
2024-02-08 16:00:36 +0100

    NMA-6100: New SatsSearchBar component (#346)

---
+++ sample-app/src/main/kotlin/com/sats/dna/sample/screens/SearchBarSampleScreen.kt
@@ -0,0 +1,75 @@
+package com.sats.dna.sample.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.sats.dna.components.SatsSearchBar
+import com.sats.dna.components.SatsSurface
+import com.sats.dna.theme.SatsTheme
+import com.sats.dna.tooling.LightDarkPreview
+
+data object SearchBarSampleScreen : SampleScreen(
+    name = "Search Bar",
+    route = "/search-bar",
+    screen = { SearchBarSampleScreen(it::navigateUp) },
+)
+
+@Composable
+private fun SearchBarSampleScreen(navigateUp: () -> Unit, modifier: Modifier = Modifier) {
+    var emptyQuery by remember { mutableStateOf("") }
+    var query by remember { mutableStateOf("SATS Carl Berner") }
+
+    ComponentScreen("Search Bar", navigateUp, modifier) { innerPadding ->
+        Column(
+            Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+                .padding(SatsTheme.spacing.m)
+                .fillMaxWidth(),
+            Arrangement.spacedBy(SatsTheme.spacing.l),
+            Alignment.CenterHorizontally,
+        ) {
+            SatsSearchBar(
+                query = emptyQuery,
+                onQueryChange = { emptyQuery = it },
+                onClearClicked = {},
+                placeholder = "Search …",
+            )
+
+            SatsSearchBar(
+                query = emptyQuery,
+                onQueryChange = { emptyQuery = it },
+                onClearClicked = {},
+                placeholder = "Search …",
+                onUpClicked = {},
+            )
+
+            SatsSearchBar(
+                query = query,
+                onQueryChange = { query = it },
+                onClearClicked = { query = "" },
+                placeholder = "Search …",
+            )
+        }
+    }
+}
+
+@LightDarkPreview
+@Composable
+private fun SearchBarSampleScreenPreview() {
+    SatsTheme {
+        SatsSurface(color = SatsTheme.colors2.backgrounds.primary.bg.default, useMaterial3 = true) {
+            SearchBarSampleScreen(navigateUp = {})
+        }
+    }
+}
