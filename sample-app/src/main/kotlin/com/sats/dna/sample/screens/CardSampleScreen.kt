438852a5e1d0725483635e54557c001d61aba521
Sindre Moen <sindrenm@gmail.com>
2024-02-21 15:07:09 +0100

    Add titled card to sample screen

--- sample-app/src/main/kotlin/com/sats/dna/sample/screens/CardSampleScreen.kt
+++ sample-app/src/main/kotlin/com/sats/dna/sample/screens/CardSampleScreen.kt
@@ -1,23 +1,23 @@
 package com.sats.dna.sample.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.sats.dna.components.SatsTitledCard
+import com.sats.dna.components.SatsTitledCardAction
 import com.sats.dna.components.card.SatsCard
 import com.sats.dna.theme.SatsTheme
 
@@ -39,27 +39,38 @@ private fun SatsCardScreen(navigateUp: () -> Unit, modifier: Modifier = Modifier
             verticalArrangement = Arrangement.spacedBy(SatsTheme.spacing.m),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            SatsCard {
-                Column(Modifier.clickable { }) {
-                    Image(
-                        painterResource(com.sats.dna.R.drawable.debug_img_placeholder),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(16f / 9)
-                            .background(SatsTheme.colors2.surfaces.fixed.bg.default),
-                        contentScale = ContentScale.Crop,
-                    )
+            SatsCard(
+                Modifier
+                    .widthIn(max = 512.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(16f / 9),
+            ) {
+                Text(
+                    text = "This is a basic SATS Card.",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(),
+                )
+            }
 
-                    Column(Modifier.padding(SatsTheme.spacing.m), Arrangement.spacedBy(SatsTheme.spacing.xxs)) {
-                        Text("SATS Card", style = SatsTheme.typography.medium.large)
-
-                        Text(
-                            "This is a SATS Card with an image, a title and a body text. It is not limited to these " +
-                                "fields, nor are any of them required; this is just one way it can be presented.",
-                        )
-                    }
-                }
+            SatsTitledCard(
+                title = "Titled Card",
+                modifier = Modifier
+                    .widthIn(max = 512.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(16f / 9),
+                action = SatsTitledCardAction.Chevron(
+                    action = {},
+                    contentDescription = "See more",
+                ),
+            ) {
+                Text(
+                    text = "This is the content of a SATS Titled Card",
+                    modifier = Modifier
+                        .padding(it)
+                        .fillMaxSize()
+                        .wrapContentSize(),
+                )
             }
         }
     }
@@ -67,7 +78,7 @@ private fun SatsCardScreen(navigateUp: () -> Unit, modifier: Modifier = Modifier
 
 @PreviewLightDark
 @Composable
-private fun Preview() {
+private fun SatsCardScreenPreview() {
     SatsTheme {
         SatsCardScreen(navigateUp = {})
     }
