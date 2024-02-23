d7776493ba77939f907055d5530d2da9522a8ae9
Cecilie Opperud <107850862+cecilieopperudsats@users.noreply.github.com>
2024-02-23 10:35:12 +0100

    NMA-6237: Create CardButton component (#359)
    Co-authored-by: Sindre Moen <sindrenm@gmail.com>

--- sample-app/src/main/kotlin/com/sats/dna/sample/screens/CardSampleScreen.kt
+++ sample-app/src/main/kotlin/com/sats/dna/sample/screens/CardSampleScreen.kt
@@ -3,10 +3,12 @@ package com.sats.dna.sample.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -18,6 +20,7 @@ import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.sats.dna.components.SatsTitledCard
 import com.sats.dna.components.SatsTitledCardAction
+import com.sats.dna.components.button.TwoOptionsInCardCardButton
 import com.sats.dna.components.card.SatsCard
 import com.sats.dna.theme.SatsTheme
 
@@ -46,7 +49,7 @@ private fun SatsCardScreen(navigateUp: () -> Unit, modifier: Modifier = Modifier
                     .aspectRatio(16f / 9),
             ) {
                 Text(
-                    text = "This is a basic SATS Card.",
+                    text = "This is a basic SATS Card",
                     modifier = Modifier
                         .fillMaxSize()
                         .wrapContentSize(),
@@ -71,6 +74,32 @@ private fun SatsCardScreen(navigateUp: () -> Unit, modifier: Modifier = Modifier
                         .fillMaxSize()
                         .wrapContentSize(),
                 )
+            }
+
+            Column {
+                SatsCard(
+                    Modifier
+                        .widthIn(max = 512.dp)
+                        .fillMaxWidth()
+                        .aspectRatio(16f / 9),
+                ) {
+                    Text(
+                        text = "This is a basic SATS Card with a Card Button",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .wrapContentSize(),
+                    )
+
+                    TwoOptionsInCardCardButton(
+                        modifier = Modifier.fillMaxHeight().wrapContentHeight(Alignment.Bottom),
+                        firstOptionOnClick = { },
+                        firstOptionText = "Add workout",
+                        firstOptionIcon = SatsTheme.icons.add,
+                        secondOptionOnClick = { },
+                        secondOptionText = "Schedule",
+                        secondOptionIcon = SatsTheme.icons.calendar,
+                    )
+                }
             }
         }
     }
