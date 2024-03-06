1a4a51b1f8676476362d591b9ecdca1ac01b5fae
Sindre Moen <sindrenm@gmail.com>
2024-03-06 19:54:09 +0100

    Tmp fix

--- sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButton.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsButton.kt
@@ -1,28 +1,16 @@
 package com.sats.dna.components.button
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.PreviewFontScale
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
 import com.sats.dna.components.SatsSurface
 import com.sats.dna.theme.SatsTheme
 
@@ -38,73 +26,16 @@ fun SatsButton(
     isLarge: Boolean = false,
     icon: Painter? = null,
 ) {
-    val isActuallyEnabled = isEnabled && !isLoading
-    val buttonColors = colors.asMaterialButtonColors(isActuallyEnabled)
-
-    val iconContent = when {
-        isLoading -> SatsButtonIconContent.Loading
-        icon != null -> SatsButtonIconContent.Icon(icon)
-        else -> SatsButtonIconContent.Empty
-    }
-
-    val border = colors.animatedBorderColor(isActuallyEnabled)?.let {
-        BorderStroke(1.dp, it)
-    }
-
-    Button(
+    SatsButton2(
         onClick = onClick,
+        label = label,
         modifier = modifier,
-        enabled = isActuallyEnabled,
-        shape = SatsTheme.shapes.roundedCorners.small,
-        border = border,
-        elevation = null,
-        colors = buttonColors,
-        contentPadding = buttonPadding(isLarge),
-    ) {
-        // Note that a horizontal arrangement is not used for spacing items, as a space-aligned arrangement would
-        // still put space between the AnimatedContent and the text, even if the AnimatedContent is empty.
-        Row(verticalAlignment = CenterVertically) {
-            AnimatedContent(iconContent, label = "Animated icon content") { iconContent ->
-                when (iconContent) {
-                    is SatsButtonIconContent.Empty -> Unit
-
-                    is SatsButtonIconContent.Icon -> {
-                        Icon(
-                            painter = iconContent.painter,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .padding(end = SatsTheme.spacing.s)
-                                .size(16.dp),
-                        )
-                    }
-
-                    is SatsButtonIconContent.Loading -> {
-                        val color = colors.animatedContentColor(isActuallyEnabled)
-
-                        CircularProgressIndicator(
-                            Modifier
-                                .padding(end = SatsTheme.spacing.s)
-                                .size(16.dp),
-                            color = color,
-                            strokeWidth = 1.5.dp,
-                        )
-                    }
-                }
-            }
-
-            Text(label, maxLines = 1)
-        }
-    }
-}
-
-@Composable
-private fun buttonPadding(isLarge: Boolean): PaddingValues {
-    val vertical = animateDpAsState(
-        if (isLarge) SatsTheme.spacing.m else SatsTheme.spacing.xs,
-        label = "Vertical padding",
+        colors = colors,
+        isEnabled = isEnabled,
+        isLoading = isLoading,
+        size = if (isLarge) SatsButtonSize.Large else SatsButtonSize.Basic,
+        icon = icon,
     )
-
-    return PaddingValues(horizontal = SatsTheme.spacing.m, vertical.value)
 }
 
 @PreviewLightDark
