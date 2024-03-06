b5edf475101b2d4ca2280cf72cda33d6ec476c72
Sindre Moen <sindrenm@gmail.com>
2024-03-06 21:23:41 +0100

    Proxy SatsButton to SatsButton2

--- sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsIconButton.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/button/SatsIconButton.kt
@@ -1,24 +1,12 @@
 package com.sats.dna.components.button
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
 import com.sats.dna.components.SatsSurface
-import com.sats.dna.internal.MaterialIcon
 import com.sats.dna.theme.SatsTheme
 
 @Deprecated("Replace with either SatsIconButton2 or SatsTopAppBarIconButton")
@@ -33,64 +21,17 @@ fun SatsIconButton(
     isLoading: Boolean = false,
     isLarge: Boolean = false,
 ) {
-    SatsIconButton(
+    SatsIconButton2(
         onClick = onClick,
-        icon = {
-            val isActuallyEnabled = isEnabled && !isLoading
-            val contentColor = colors.animatedContentColor(isActuallyEnabled)
-
-            MaterialIcon(icon, contentDescription = null, Modifier.size(24.dp), contentColor)
-        },
+        icon = icon,
         onClickLabel = onClickLabel,
         modifier = modifier,
         colors = colors,
         isEnabled = isEnabled,
         isLoading = isLoading,
-        isLarge = isLarge,
+        size = if (isLarge) SatsButtonSize.Large else SatsButtonSize.Basic,
     )
 }
-
-@Composable
-private fun SatsIconButton(
-    onClick: () -> Unit,
-    icon: @Composable () -> Unit,
-    onClickLabel: String?,
-    modifier: Modifier = Modifier,
-    colors: SatsButtonColor = SatsButtonColor.Primary,
-    isEnabled: Boolean = true,
-    isLoading: Boolean = false,
-    isLarge: Boolean = false,
-) {
-    val isActuallyEnabled = isEnabled && !isLoading
-    val backgroundColor = colors.animatedContainerColor(isEnabled)
-
-    Box(
-        modifier = modifier
-            .clip(SatsTheme.shapes.roundedCorners.small)
-            .background(backgroundColor)
-            .clickable(isActuallyEnabled, onClickLabel, Role.Button, onClick),
-        contentAlignment = Alignment.Center,
-    ) {
-        AnimatedContent(isLoading, Modifier.padding(animatedPadding(isLarge)), label = "Loading state") { isLoading ->
-            if (isLoading) {
-                CircularProgressIndicator(
-                    modifier = Modifier.size(24.dp),
-                    color = colors.contentColor,
-                    strokeWidth = 2.dp,
-                )
-            } else {
-                icon()
-            }
-        }
-    }
-}
-
-@Composable
-private fun animatedPadding(isLarge: Boolean) =
-    animateDpAsState(
-        if (isLarge) SatsTheme.spacing.m else SatsTheme.spacing.xs,
-        label = "Padding",
-    ).value
 
 @PreviewLightDark
 @Composable
