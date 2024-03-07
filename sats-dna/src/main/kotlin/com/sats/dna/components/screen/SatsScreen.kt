7a3511467c73be3d66878d199073e7b625ef6288
Sindre Moen <sindrenm@gmail.com>
2024-03-07 13:43:25 +0100

    Add missing protein bar result typealias

--- sats-dna/src/main/kotlin/com/sats/dna/components/screen/SatsScreen.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/screen/SatsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.SnackbarVisuals
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -25,6 +26,7 @@ import com.sats.dna.theme.SatsTheme
 
 typealias ProteinBarDuration = SnackbarDuration
 typealias ProteinBarHostState = SnackbarHostState
+typealias ProteinBarResult = SnackbarResult
 typealias ProteinBarVisuals = SnackbarVisuals
 
 @Composable
