1edae47c43480525d8db5af9f001e73f35b715f2
Sindre Moen <sindrenm@gmail.com>
2024-03-29 20:49:49 +0100

    Delete old color system

--- sats-dna/src/main/kotlin/com/sats/dna/theme/SatsTheme.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/theme/SatsTheme.kt
@@ -8,11 +8,8 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
-import com.sats.dna.colors.SatsColors
 import com.sats.dna.colors.SatsColors2
-import com.sats.dna.colors.SatsDarkColors
 import com.sats.dna.colors.SatsDarkColors2
-import com.sats.dna.colors.SatsLightColors
 import com.sats.dna.colors.SatsLightColors2
 import com.sats.dna.shapes.SatsShapes
 import com.sats.dna.spacing.SatsSpacing
@@ -26,20 +23,18 @@ import androidx.compose.material3.Typography as Material3Typography
 
 @Composable
 fun SatsTheme(content: @Composable () -> Unit) {
-    val colors = if (isSystemInDarkTheme()) SatsDarkColors else SatsLightColors
     val colors2 = if (isSystemInDarkTheme()) SatsDarkColors2 else SatsLightColors2
 
     CompositionLocalProvider(
-        LocalSatsColors provides colors,
         LocalSatsColors2 provides colors2,
     ) {
         Material2Theme(
-            colors = LocalSatsColors.current.toMaterial2(),
+            colors = LocalSatsColors2.current.toMaterial2(),
             typography = LocalSatsTypography.current.toMaterial2(),
         ) {
             ProvideMaterial2TextStyle(SatsTheme.typography.normal.basic) {
                 Material3Theme(
-                    colorScheme = LocalSatsColors.current.toMaterial3(),
+                    colorScheme = LocalSatsColors2.current.toMaterial3(),
                     typography = LocalSatsTypography.current.toMaterial3(),
                 ) {
                     ProvideMaterial3TextStyle(SatsTheme.typography.normal.basic) {
@@ -52,11 +47,6 @@ fun SatsTheme(content: @Composable () -> Unit) {
 }
 
 object SatsTheme {
-    @Suppress("DeprecatedCallableAddReplaceWith")
-    @Deprecated("Use colors from colors2 instead")
-    val colors: SatsColors
-        @Composable get() = LocalSatsColors.current
-
     val colors2: SatsColors2
         @Composable get() = LocalSatsColors2.current
 
@@ -70,24 +60,15 @@ object SatsTheme {
         @Composable get() = LocalSatsShapes.current
 }
 
-internal val LocalSatsColors = staticCompositionLocalOf { SatsLightColors }
 internal val LocalSatsColors2 = staticCompositionLocalOf { SatsLightColors2 }
 internal val LocalSatsTypography = staticCompositionLocalOf { SatsTypography }
 internal val LocalSatsSpacing = staticCompositionLocalOf { SatsSpacing }
 internal val LocalSatsShapes = staticCompositionLocalOf { SatsShapes }
 
-private fun SatsColors.toMaterial2() = if (isLightMode) {
-    lightColors()
-} else {
-    darkColors()
-}
+private fun SatsColors2.toMaterial2() = if (isLightMode) lightColors() else darkColors()
 
 private fun SatsTypography.toMaterial2() = Material2Typography()
 
-private fun SatsColors.toMaterial3() = if (isLightMode) {
-    lightColorScheme()
-} else {
-    darkColorScheme()
-}
+private fun SatsColors2.toMaterial3() = if (isLightMode) lightColorScheme() else darkColorScheme()
 
 private fun SatsTypography.toMaterial3() = Material3Typography()
