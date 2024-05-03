115ca03c8d594b85261a4772e333915be7800da9
Sindre Moen <sindrenm@gmail.com>
2024-05-03 16:20:50 +0200

    Extract ComponentScreen to its own file

--- sample-app/src/main/kotlin/com/sats/dna/sample/screens/SampleScreen.kt
+++ sample-app/src/main/kotlin/com/sats/dna/sample/screens/SampleScreen.kt
@@ -1,24 +1,11 @@
 package com.sats.dna.sample.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.sats.dna.SatsIcons
-import com.sats.dna.components.appbar.SatsTopAppBar
-import com.sats.dna.components.button.SatsTopAppBarIconButton
-import com.sats.dna.components.screen.SatsScreen
-import com.sats.dna.icons.Back
 import com.sats.dna.sample.internal.LocalAnimatedContentScope
-import com.sats.dna.sample.internal.trySharedBounds
 
 sealed class SampleScreen(
     val name: String,
@@ -33,36 +20,4 @@ sealed class SampleScreen(
             }
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-internal fun ComponentScreen(
-    title: String,
-    navigateUp: () -> Unit,
-    modifier: Modifier = Modifier,
-    bottomBar: @Composable () -> Unit = { Box(Modifier.navigationBarsPadding()) },
-    content: @Composable (innerPadding: PaddingValues) -> Unit,
-) {
-    val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
-
-    SatsScreen(
-        modifier = modifier.nestedScroll(topAppBarScrollBehavior.nestedScrollConnection),
-        topBar = {
-            SatsTopAppBar(
-                navigationIcon = {
-                    SatsTopAppBarIconButton(
-                        onClick = navigateUp,
-                        icon = SatsIcons.Back,
-                        onClickLabel = null,
-                    )
-                },
-                title = title,
-                scrollBehavior = topAppBarScrollBehavior,
-                modifier = Modifier.trySharedBounds("top-bar"),
-            )
-        },
-        bottomBar = bottomBar,
-        content = content,
-    )
 }
