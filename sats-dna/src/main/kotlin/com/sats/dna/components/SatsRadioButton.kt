00cd0400a64324266b81918bb2bb1f38396d6ce8
Guro Ørbech <guro.olsen@gmail.com>
2024-06-07 12:40:52 +0200

    Add Fixed color scheme to radio buttons

--- sats-dna/src/main/kotlin/com/sats/dna/components/SatsRadioButton.kt
+++ sats-dna/src/main/kotlin/com/sats/dna/components/SatsRadioButton.kt
@@ -13,21 +13,40 @@ fun SatsRadioButton(
     selected: Boolean,
     onClick: (() -> Unit)?,
     modifier: Modifier = Modifier,
+    color: SatsRadioButtonColors = SatsRadioButtonColors.Primary,
     enabled: Boolean = true,
 ) {
-    RadioButton(selected, onClick, modifier, enabled, colors = colors)
+    RadioButton(selected, onClick, modifier, enabled, colors = color.colors)
 }
 
-private val colors: RadioButtonColors
-    @Composable get() {
-        val primary = SatsTheme.colors.graphicalElements.selector.primary
+enum class SatsRadioButtonColors {
+    Primary,
+    Fixed,
+}
 
-        return RadioButtonDefaults.colors(
-            selectedColor = primary.selected.default.bg,
-            unselectedColor = primary.unselected.default.outline,
-            disabledSelectedColor = primary.selected.disabled.bg,
-            disabledUnselectedColor = primary.unselected.disabled.outline,
-        )
+private val SatsRadioButtonColors.colors: RadioButtonColors
+    @Composable get() = when (this) {
+        SatsRadioButtonColors.Primary -> {
+            val primary = SatsTheme.colors.graphicalElements.selector.primary
+
+            RadioButtonDefaults.colors(
+                selectedColor = primary.selected.default.bg,
+                unselectedColor = primary.unselected.default.outline,
+                disabledSelectedColor = primary.selected.disabled.bg,
+                disabledUnselectedColor = primary.unselected.disabled.outline,
+            )
+        }
+
+        SatsRadioButtonColors.Fixed -> {
+            val fixed = SatsTheme.colors.graphicalElements.selectorFixed
+
+            RadioButtonDefaults.colors(
+                selectedColor = fixed.selected.default.bg,
+                unselectedColor = fixed.unselected.default.outline,
+                disabledSelectedColor = fixed.selected.disabled.bg,
+                disabledUnselectedColor = fixed.unselected.disabled.outline,
+            )
+        }
     }
 
 @PreviewLightDark
@@ -66,6 +85,64 @@ private fun DisabledUnselectedPreview() {
     SatsTheme {
         SatsSurface(color = SatsTheme.colors.backgrounds.primary.default.bg) {
             SatsRadioButton(selected = false, onClick = {}, enabled = false)
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun EnabledSelectedFixedPreview() {
+    SatsTheme {
+        SatsSurface(color = SatsTheme.colors.backgrounds.fixed.primary.default.bg) {
+            SatsRadioButton(
+                selected = true,
+                color = SatsRadioButtonColors.Fixed,
+                onClick = {},
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun EnabledUnselectedFixedPreview() {
+    SatsTheme {
+        SatsSurface(color = SatsTheme.colors.backgrounds.fixed.primary.default.bg) {
+            SatsRadioButton(
+                selected = false,
+                color = SatsRadioButtonColors.Fixed,
+                onClick = {},
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun DisabledSelectedFixedPreview() {
+    SatsTheme {
+        SatsSurface(color = SatsTheme.colors.backgrounds.fixed.primary.default.bg) {
+            SatsRadioButton(
+                selected = true,
+                onClick = {},
+                color = SatsRadioButtonColors.Fixed,
+                enabled = false,
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun DisabledUnselectedFixedPreview() {
+    SatsTheme {
+        SatsSurface(color = SatsTheme.colors.backgrounds.fixed.primary.default.bg) {
+            SatsRadioButton(
+                selected = false,
+                onClick = {},
+                color = SatsRadioButtonColors.Fixed,
+                enabled = false,
+            )
         }
     }
 }
