f08908a478217e2b6b4c409207350f537f1d664e
Guro Ørbech <48335680+gurooerbech@users.noreply.github.com>
2024-06-10 08:48:29 +0200

    NMA-6806: Add Fixed color variant to SatsRadioButtons (#492)

--- sample-app/src/main/kotlin/com/sats/dna/sample/screens/components/RadioButtonsSampleScreen.kt
+++ sample-app/src/main/kotlin/com/sats/dna/sample/screens/components/RadioButtonsSampleScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
@@ -12,6 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import com.sats.dna.components.SatsRadioButton
+import com.sats.dna.components.SatsRadioButtonColors
+import com.sats.dna.components.SatsSurface
 import com.sats.dna.sample.screens.SampleScreen
 import com.sats.dna.theme.SatsTheme
 
@@ -26,21 +29,94 @@ fun RadioButtonsSampleScreen(navigateUp: () -> Unit, modifier: Modifier = Modifi
                 .wrapContentSize(),
             Arrangement.spacedBy(SatsTheme.spacing.m, Alignment.CenterVertically),
         ) {
-            LabeledRadioButton("Enabled, unselected", enabled = true, selected = false)
-            LabeledRadioButton("Enabled, selected", enabled = true, selected = true)
-            LabeledRadioButton("Disabled, unselected", enabled = false, selected = false)
-            LabeledRadioButton("Disabled, selected", enabled = false, selected = true)
+            Section("Primary colors") {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(SatsTheme.spacing.xs),
+                ) {
+                    LabeledRadioButton("Enabled, unselected", enabled = true, selected = false)
+                    LabeledRadioButton("Enabled, selected", enabled = true, selected = true)
+                    LabeledRadioButton("Disabled, unselected", enabled = false, selected = false)
+                    LabeledRadioButton("Disabled, selected", enabled = false, selected = true)
+                }
+            }
+            Section(
+                label = "Fixed colors",
+                isFixedBackground = true,
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(SatsTheme.spacing.xs),
+                ) {
+                    LabeledRadioButton(
+                        label = "Enabled, unselected",
+                        enabled = true,
+                        colors = SatsRadioButtonColors.Fixed,
+                        selected = false,
+                    )
+                    LabeledRadioButton(
+                        label = "Enabled, selected",
+                        enabled = true,
+                        colors = SatsRadioButtonColors.Fixed,
+                        selected = true,
+                    )
+                    LabeledRadioButton(
+                        label = "Disabled, unselected",
+                        enabled = false,
+                        colors = SatsRadioButtonColors.Fixed,
+                        selected = false,
+                    )
+                    LabeledRadioButton(
+                        label = "Disabled, selected",
+                        enabled = false,
+                        colors = SatsRadioButtonColors.Fixed,
+                        selected = true,
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-private fun LabeledRadioButton(label: String, enabled: Boolean, selected: Boolean) {
+private fun Section(
+    label: String,
+    isFixedBackground: Boolean = false,
+    content: @Composable () -> Unit,
+) {
+    val color = if (isFixedBackground) {
+        SatsTheme.colors.backgrounds.fixed.primary.default.bg
+    } else {
+        SatsTheme.colors.surfaces.primary.default.bg
+    }
+
+    SatsSurface(Modifier.fillMaxWidth(), color = color, shape = SatsTheme.shapes.roundedCorners.medium) {
+        Column(
+            modifier = Modifier.padding(SatsTheme.spacing.s),
+            verticalArrangement = Arrangement.spacedBy(SatsTheme.spacing.xs),
+        ) {
+            Text(label, style = SatsTheme.typography.satsHeadlineEmphasis.small)
+
+            content()
+        }
+    }
+}
+
+@Composable
+private fun LabeledRadioButton(
+    label: String,
+    enabled: Boolean,
+    selected: Boolean,
+    colors: SatsRadioButtonColors = SatsRadioButtonColors.Primary,
+) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(SatsTheme.spacing.s),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        SatsRadioButton(selected, null, enabled = enabled)
+        SatsRadioButton(
+            selected = selected,
+            onClick = null,
+            color = colors,
+            enabled = enabled,
+        )
 
         Text(label)
     }
